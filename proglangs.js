c39fa3c82afb8483083c5b7abc355e5a55b0f72f
afykerud <andersfykerud@hotmail.com>
2013-09-28 17:01:15 +0200

    Update proglangs.js
    Lagt til så det blir sendt et varsel nå man trykker på linkene. pluss man får opp hva man har valgt av prog språk

--- proglangs.js
+++ proglangs.js
@@ -5,7 +5,12 @@ alert(element_unordered_list);
 function printToOutputBox(selectobj) {
 var e_proglangs = document.getElementById("proglangs");
 var e_outputbox = document.getElementById("outputbox");
-e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.	
+e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.	// Forandrer innholdet i outputbox
 options[selectobj.selectedIndex].text;
 
+}
+
+function myFunction()
+{
+alert("Beklager så mye, denne siden er under konstruksjon!");	// Gjør at funksjonen sender et varsel.
 }
